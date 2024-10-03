028368a5f6c6151bf3f10463b7abc16533215181
fawwazaid <fawwazaid@gmail.com>
2024-10-03 20:05:10 +0700

    edit deplo.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,20 @@ if command -v zsh >/dev/null 2>&1; then
     echo "Using zsh to run the commands..."
     # If zsh is available, run the Docker commands in zsh
     exec zsh -c "
-        docker build -t ecommerce .
-        docker tag ecommerce dockerfawwazaid/ecommerce:final-14
-        docker push dockerfawwazaid/ecommerce:final-14
+        docker build -t ecomarket .
+        docker tag ecomarket dockerfawwazaid/ecomarket:latest
+        docker push dockerfawwazaid/ecomarket:latest
     "
 else
     # If zsh is not available, fall back to bash
     echo "zsh not found. Using bash instead..."
     exec bash -c "
-        docker build -t ecommerce .
-        docker tag ecommerce dockerfawwazaid/ecommerce:final-14
-        docker push dockerfawwazaid/ecommerce:final-14
+        docker build -t ecomarket .
+        docker tag ecomarket dockerfawwazaid/ecomarket:latest
+        docker push dockerfawwazaid/ecomarket:latest
     "
 fi  # End of the if block
+
+# run
+# chmod +x deploy.sh
+# .deploy.sh
